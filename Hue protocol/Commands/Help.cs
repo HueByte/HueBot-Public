07d6719e65f36caa6d571603c088f75472245708
Denis <69825463+DenVot@users.noreply.github.com>
2021-02-28 18:39:30 +0300

    Removing some using-s and optimised EmbedBuilder-s

--- Hue protocol/Commands/Help.cs
+++ Hue protocol/Commands/Help.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,28 +6,29 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace HueProtocol.Commands
 {
     [Name("Others")]
     public class Help : ModuleBase<SocketCommandContext>
     {
-        private CommandService _commands;
-        private readonly ILogger<Help> _logger;
+        private CommandService _commands;        
         private DiscordShardedClient _client;
 
-        public Help(CommandService commands, DiscordShardedClient client, ILogger<Help> logger)
+        public Help(CommandService commands, DiscordShardedClient client)
         {
             _commands = commands;
-            _client = client;
-            _logger = logger;
+            _client = client;            
         }
 
         [Command("HelpOld", RunMode = RunMode.Async)]
         public async Task HelpMe()
         {
+            var color = Color.Purple; //Default color for embed
+            var avatarUrl = _client.CurrentUser.GetAvatarUrl();//Client avatar
+            var creatorAvatar = _client.GetUser(215556401467097088).GetAvatarUrl();
+            string creatorUsername = _client.GetUser(215556401467097088).Username;
+
             //Get commands from modules
             var AdminCommands = _commands.Commands.Where(x => x.Module.Name.ToString() == "Admin").ToList();
             var FunCommands = _commands.Commands.Where(x => x.Module.Name.ToString() == "Fun").ToList();
@@ -36,13 +37,22 @@ namespace HueProtocol.Commands
 
             //Get user and create embed
             IUser creator = _client.GetUser(215556401467097088);
-            var embed = new EmbedBuilder();
-            embed.WithAuthor(creator.Username, creator.GetAvatarUrl());
-            embed.Title = "Hue comes to help";
-            embed.Description = $"Command list";
-            embed.ThumbnailUrl = $"{_client.CurrentUser.GetAvatarUrl()}";
-            embed.WithColor(Color.DarkPurple);
-            embed.WithFooter("Bot made with love by Hue 🤍");
+            var embed = new EmbedBuilder
+            { 
+                Author = new EmbedAuthorBuilder
+                { 
+                    Name = creatorUsername,
+                    IconUrl = creatorAvatar
+                },
+                Title = "Hue comes to help",
+                Description = "Command list",
+                ThumbnailUrl = _client.CurrentUser.GetAvatarUrl(),
+                Color = color,
+                Footer = new EmbedFooterBuilder
+                { 
+                    Text = "Bot made with love by Hue 🤍"
+                }
+            };
 
             #region Command Loops
 
@@ -79,7 +89,30 @@ namespace HueProtocol.Commands
 
             try
             {
-                var emb = new EmbedBuilder();
+                var emb = new EmbedBuilder
+                {
+                    ThumbnailUrl = avatarUrl,
+                    Color = color,
+                    Title = "Your help is here! I've sent you quick command list to DM's",
+                    Timestamp = DateTimeOffset.Now,
+                    Footer = new EmbedFooterBuilder
+                    { 
+                        Text = $"Help requested by {Context.User.Username}"
+                    },
+                    Fields = new List<EmbedFieldBuilder>
+                    { 
+                        new EmbedFieldBuilder
+                        { 
+                            Name = "Website:",
+                            Value = "https://huebyte.github.io/Bot"
+                        },
+                        new EmbedFieldBuilder
+                        { 
+                            Name = "Invite link:",
+                            Value = "https://discord.com/api/oauth2/authorize?client_id=704857702953779210&permissions=8&scope=bot"
+                        }
+                    }
+                };
                 emb.WithThumbnailUrl(_client.CurrentUser.GetAvatarUrl());
                 emb.WithColor(Color.DarkPurple);
                 emb.WithTitle("Your help is here! I've sent you quick command list to DM's");
@@ -101,19 +134,45 @@ namespace HueProtocol.Commands
         [Summary("Shows the help you can also use $Help <command>")]
         public async Task Helper(string command = null)
         {
+            var color = Color.Purple; //Default color for embed
+            var avatarUrl = _client.CurrentUser.GetAvatarUrl();//Client avatar
+            var creatorAvatar = _client.GetUser(215556401467097088).GetAvatarUrl();
+            string creatorUsername = _client.GetUser(215556401467097088).Username;
+
             if (command == null)
             {
                 try
                 {
-                    var emb = new EmbedBuilder();
-                    emb.WithThumbnailUrl(_client.CurrentUser.GetAvatarUrl());
-                    emb.WithColor(Color.DarkPurple);
-                    emb.WithTitle("Your help is here!");
-                    emb.WithCurrentTimestamp();
-                    emb.WithFooter($"Help requested by {Context.User.Username}");
-                    emb.AddField("Website: ", "https://huebyte.github.io/Bot");
-                    emb.AddField("Invite link: ", "https://discord.com/api/oauth2/authorize?client_id=704857702953779210&permissions=2146958454&scope=bot");
-                    emb.AddField("Additional info", "You can also use the $help <command> for details about specific command");
+
+                    var emb = new EmbedBuilder
+                    {
+                        ThumbnailUrl = avatarUrl,
+                        Color = color,
+                        Title = "Your help is here! I've sent you quick command list to DM's",
+                        Timestamp = DateTimeOffset.Now,
+                        Footer = new EmbedFooterBuilder
+                        { 
+                            Text = $"Help requested by {Context.User.Username}"
+                        },
+                        Fields = new List<EmbedFieldBuilder>
+                        { 
+                            new EmbedFieldBuilder
+                            { 
+                                Name = "Website:",
+                                Value = "https://huebyte.github.io/Bot"
+                            },
+                            new EmbedFieldBuilder
+                            { 
+                                Name = "Invite link:",
+                                Value = "https://discord.com/api/oauth2/authorize?client_id=704857702953779210&permissions=8&scope=bot"
+                            },
+                            new EmbedFieldBuilder
+                            { 
+                                Name = "Additional info",
+                                Value = "You can also use the $help <command> for details about specific command"
+                            }
+                        }
+                    };                    
                     await ReplyAsync("", false, emb.Build());
                 }
                 catch
